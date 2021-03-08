82dc72b8e9c1e492494e9f4594c5e91c058b72ab
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-03-08 11:37:38 +0100

    added type suggestions for Model

--- promiseless/Model.py
+++ promiseless/Model.py
@@ -1,19 +1,22 @@
 import numpy as np
+from layer import InputLayer, HiddenLayer
+from loss import LossFunction
 
 
 class Model:
-    def __init__(self, input_layer, layers, loss_function):
+    def __init__(self, input_layer: InputLayer, layers: list[HiddenLayer], loss_function: LossFunction):
         self._input_layer = input_layer
         self._layers = layers
         self._loss_function = loss_function
         self._training_history = np.empty(0)
 
-    def train(self, x_train, y_train, bias, batch_size, epochs, learning_rate, momentum, evaluation_dataset=None):
+    def train(self, x_train: np.ndarray, y_train: np.ndarray, bias, batch_size, epochs, learning_rate, momentum, evaluation_dataset=None):
         for i, x in enumerate(x_train):
+            data = x
             for j, layer in enumerate(self._layers):
-                layer.feedforward(x)
-            loss = self._loss_function.calculate(x, y_train)
-            error = self._loss_function.derivative(x, y_train)
+                data = layer.feedforward(data)
+            loss = self._loss_function.calculate(data, y_train[i])
+            error = self._loss_function.derivative(data, y_train[i])
 
     def predict(self, x_test, y_test):
         pass
