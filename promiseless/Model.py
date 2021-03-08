4ffccba10e028cc431a27a76c46bc596e33f5cbb
boguckiw <boguckiw@student.mini.pw.edu.pl>
2021-03-08 14:00:33 +0100

    Added batching

--- promiseless/Model.py
+++ promiseless/Model.py
@@ -10,13 +10,27 @@ class Model:
         self._loss_function = loss_function
         self._training_history = np.empty(0)
 
+    @staticmethod
+    def __create_batches(df, batch_size):
+        '''
+        :param df: numpy array
+        :param batch_size: size of the batches
+        :return: list of numpy arrays
+        '''
+        n = len(df)
+        new_df = df[np.random.permutation(n)]
+        batches = [new_df[start:start + batch_size] for start in range(0, n, batch_size)]
+        return batches
+
     def train(self, x_train: np.ndarray, y_train: np.ndarray, bias, batch_size, epochs, learning_rate, momentum, evaluation_dataset=None):
-        for i, x in enumerate(x_train):
-            data = x
-            for j, layer in enumerate(self._layers):
-                data = layer.feedforward(data)
-            loss = self._loss_function.calculate(data, y_train[i])
-            error = self._loss_function.derivative(data, y_train[i])
+        batches = self.__create_batches(x_train, batch_size)
+        for batch in batches:
+            for i, x in enumerate(batch):
+                data = x
+                for j, layer in enumerate(self._layers):
+                    data = layer.feedforward(data)
+                loss = self._loss_function.calculate(data, y_train[i])
+                error = self._loss_function.derivative(data, y_train[i])
 
     def predict(self, x_test, y_test):
         pass
--- test.py
+++ test.py
@@ -15,8 +15,18 @@ model = architecture.build_model()
 name = "data.simple.test.100"
 data = pd.read_csv("C:\\Users\\wojte\\OneDrive\\Dokumenty\\Studia\\Deep Learning\\projekt1\\classification\\{}.csv".format(name))
 x_train = data.loc[:, ["x","y"]]
+x_train = np.array(x_train)
 
 def create_batches(df, batch_size):
+    '''
+    :param df: numpy array
+    :param batch_size: size of the batches
+    :return: list of numpy arrays
+    '''
     n = len(df)
-    new_df = data.iloc[np.random.permutation(n)]
-    batches = [new_df.iloc[]]
+    new_df = df[np.random.permutation(n)]
+    batches = [new_df[start:start + batch_size] for start in range(0, n, batch_size)]
+    return batches
+
+
+batches = create_batches(x_train, 1)
