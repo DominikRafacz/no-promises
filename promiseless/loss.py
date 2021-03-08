5b4afd2b8e9cd2238174ca6547c727c798a931f4
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-03-08 11:37:27 +0100

    added superclass for LossFunction

--- promiseless/loss.py
+++ promiseless/loss.py
@@ -1,3 +1,16 @@
-class MSE:
+class LossFunction:
     def __init__(self):
         pass
+
+    @staticmethod
+    def calculate(prediction, target):
+        pass
+
+    @staticmethod
+    def derivative(prediction, target):
+        pass
+
+
+class MSE(LossFunction):
+    def __init__(self):
+        super().__init__()
