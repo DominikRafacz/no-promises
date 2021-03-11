16cf65e700647095bb92a4eca7b83d8506c8ea70
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-03-11 18:03:00 +0100

    added MAE and Crossentropy

--- promiseless/loss.py
+++ promiseless/loss.py
@@ -19,3 +19,23 @@ class MSE(LossFunction):
     @staticmethod
     def derivative(prediction: numpy.ndarray, target: numpy.ndarray):
         return 2 * (prediction - target) / prediction.shape[0]
+
+
+class MAE(LossFunction):
+    @staticmethod
+    def calculate(prediction: numpy.ndarray, target: numpy.ndarray):
+        return numpy.sum(numpy.abs(prediction - target), axis=0) / prediction.shape[0]
+
+    @staticmethod
+    def derivative(prediction: numpy.ndarray, target: numpy.ndarray):
+        return numpy.where(prediction - target >= 0, 1, -1) / prediction.shape[0]
+
+
+class CategoricalCrossEntropy(LossFunction):
+    @staticmethod
+    def calculate(prediction: numpy.ndarray, target: numpy.ndarray):
+        return -numpy.sum(target * numpy.log(1e-15 + prediction), axis=0) / prediction.shape[0]
+
+    @staticmethod
+    def derivative(prediction: numpy.ndarray, target: numpy.ndarray):
+        return -numpy.sum(prediction / target, axis=0) / prediction.shape[0]
