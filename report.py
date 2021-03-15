b7d3169ddf8967d21b91324b94128497e2962f1a
boguckiw <boguckiw@student.mini.pw.edu.pl>
2021-03-15 12:39:59 +0100

    Added loops for creating models

--- report.py
+++ report.py
@@ -1,10 +1,9 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from promiseless.Architecture import Architecture
 from promiseless.layer import InputLayer, HiddenLayer
-from promiseless.activation import Sigmoid, ReLU, Tanh, Softmax
-from promiseless.loss import CategoricalCrossEntropy
+from promiseless.activation import Sigmoid, ReLU, Tanh, Softmax, LinearActivation
+from promiseless.loss import CategoricalCrossEntropy, MSE, MAE
 from promiseless.functions import read_data, visualize_loss, visualize_results
 
 
@@ -30,35 +29,60 @@ mdl.train(x_train, y_train, batch_size=100, learning_rate=10e-4, epochs=100, eva
 
 res, loss = mdl.predict(x_test, y_test)
 
-np.random.seed(123)
+task = "regression"
+activations = [Tanh, ReLU, Sigmoid]
+batches = [1, 10, 100, 500]
+hidden_layers = [1, 2, 3, 5]
+loss_functions = [MSE, MAE]
+layer_sizes = [2, 5, 10]
 
-mdl_classif = Architecture()\
-    .add_input_layer(InputLayer(2))\
-    .add_layer(HiddenLayer(5, activation=Sigmoid))\
-    .add_layer(HiddenLayer(2, activation=Softmax))\
-    .set_loss_function(CategoricalCrossEntropy)\
-    .build_model()
+for hidden_layer in hidden_layers:
+    for activation in activations:
+        for loss_function in loss_functions:
+            for batch in batches:
+                for layer_size in layer_sizes:
+                    i = 0
+                    np.random.seed(123)
+                    mdl = Architecture().add_input_layer(InputLayer(1))
+                    for _ in range(hidden_layer):
+                        mdl = mdl.add_layer(HiddenLayer(layer_size, activation=activation))
+                    mdl = mdl.set_loss_function(loss_function=loss_function).build_model()
+                    mdl.train(x_train, y_train, batch_size=batch, learning_rate=10e-4, epochs=100)
+                    res, loss = mdl.predict(x_test, y_test)
+                    visualize_results(x_test, res, y_test, "regression", "test{}".format(i))
+                    visualize_loss(mdl, "test{}".format(i))
+                    i += 1
 
-mdl_classif.train(x_train2, y_train2, batch_size=100, learning_rate=10e-4, epochs=100)
+# np.random.seed(123)
+#
+# mdl_classif = Architecture()\
+#     .add_input_layer(InputLayer(2))\
+#     .add_layer(HiddenLayer(5, activation=Sigmoid))\
+#     .add_layer(HiddenLayer(2, activation=Softmax))\
+#     .set_loss_function(CategoricalCrossEntropy)\
+#     .build_model()
+#
+# mdl_classif.train(x_train2, y_train2, batch_size=100, learning_rate=10e-4, epochs=100)
+#
+# res2, loss2 = mdl_classif.predict(x_train2, y_train2, return_class=True)
 
-res2, loss2 = mdl_classif.predict(x_train2, y_train2, return_class=True)
 
 
 
 
-
+visualize_results(x_test, res, y_test, "regression", "test2")
 visualize_results(x_test, res, y_test, "regression")
-visualize_results(x_train2, res2, y_train2, "classification")
+# visualize_results(x_train2, res2, y_train2, "classification")
 
 
 
 
 
-visualize_loss(mdl)
-
-plt.scatter(x_test, res)
-plt.show()
-print(mdl.training_history)
-
-plt.plot(range(1,101),mdl.training_history[0])
-plt.plot(range(1,101),mdl.training_history[1])
+visualize_loss(mdl, "test")
+#
+# plt.scatter(x_test, res)
+# plt.show()
+# print(mdl.training_history)
+#
+# plt.plot(range(1,101),mdl.training_history[0])
+# plt.plot(range(1,101),mdl.training_history[1])
