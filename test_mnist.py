6d8a7da39f9a29f99a168f6f95cfa0390c09b5ac
boguckiw <boguckiw@student.mini.pw.edu.pl>
2021-03-22 22:51:58 +0100

    Testing mnist new version

--- test_mnist.py
+++ test_mnist.py
@@ -1,53 +1,65 @@
 import numpy as np
 import pandas as pd
+import time
 from promiseless.architecture import Architecture
 from promiseless.layer import InputLayer, HiddenLayer
-from promiseless.activation import Tanh, LinearActivation, Softmax, Sigmoid
-from promiseless.util import read_data, visualize_loss, visualize_results
+from promiseless.activation import Tanh, LinearActivation, Softmax, Sigmoid, ReLU
+from promiseless.util import read_data, visualize_loss, visualize_results, transform, accuracy
 from promiseless.loss import CategoricalCrossEntropy
+from promiseless.initialization import XavierInitialization
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("data/classification/mnist.train.csv")
 y = np.array(pd.get_dummies(data.loc[:, "label"]))
 x = np.array(data.iloc[:, 1:])
+# x, x_mean, x_std = transform(x)
+x = (x - x.mean())/x.std()
 
 
-def transform(x, x_mean=None, x_std=None):
-       if x_mean is None:
-              x_mean = x.mean(axis=0)
-              x_std = x.std(axis=0)
-       x_std_nonzero = x_std > 1e-4
-       x = x - x_mean
-       x[:, x_std_nonzero] = x[:, x_std_nonzero] / x_std[x_std_nonzero]
-       return x, x_mean, x_std
-
-x, x_mean, x_std = transform(x)
-
-data_test = pd.read_csv("data/classification/mnist.test.csv")
-x_test = np.array(data_test)
-x_test = transform(x_test, x_mean, x_std)
+# data_test = pd.read_csv("data/classification/mnist.test.csv")
+# x_test = np.array(data_test)
+# x_test = transform(x_test, x_mean, x_std)
 
 np.random.seed(69)
-idx = np.random.choice(np.arange(0, 42000), size=10000, replace=False)
-x_train = x[idx, :]
-y_train = y[idx, :]
+idx = np.random.choice(np.arange(0, 42000), size=42000, replace=False)
+x_train = x[idx[:40000], :]
+y_train = y[idx[:40000], :]
+x_valid = x[idx[40000:], :]
+y_valid = y[idx[40000:], :]
 
 np.random.seed(2137)
 mdl = (Architecture()
        .add_input_layer(InputLayer(784))
-       .add_layer(HiddenLayer(32, activation=Sigmoid))
-       .add_layer(HiddenLayer(32, activation=Sigmoid))
+       .add_layer(HiddenLayer(100, activation=Sigmoid))
        .add_layer(HiddenLayer(10, activation=Softmax))
        .set_loss_function(CategoricalCrossEntropy)
        .build_model())
 
-mdl.train(x_train, y_train, batch_size=64, epochs=100, learning_rate=0.0001)
+t1 = time.time()
+mdl.train(x_train, y_train, batch_size=400, epochs=200, learning_rate=0.001, momentum_lambda=0.9,
+          evaluation_dataset=(x_valid, y_valid))
+t2 = time.time()
+print("Time: {0:.4f}".format(t2 - t1))
+visualize_loss(mdl)
+res, loss = mdl.predict(x_valid, y_valid, return_class=True)
 
-res, loss = mdl.predict(x_train, y_train, return_class=True)
+np.random.seed(2137)
+mdl2 = (Architecture()
+        .add_input_layer(InputLayer(784))
+        .add_layer(HiddenLayer(256, activation=Sigmoid))
+        .add_layer(HiddenLayer(128, activation=Sigmoid))
+        .add_layer(HiddenLayer(64, activation=Sigmoid))
+        .add_layer(HiddenLayer(10, activation=Softmax))
+        .set_loss_function(CategoricalCrossEntropy)
+        .set_initialization_method(XavierInitialization)
+        .build_model())
 
+t1 = time.time()
+mdl2.train(x_train, y_train, batch_size=50, epochs=5, learning_rate=0.001, momentum_lambda=0.9,
+           evaluation_dataset=(x_valid, y_valid))
+t2 = time.time()
+print("Time: {0:.4f}".format(t2 - t1))
+visualize_loss(mdl2)
 
-def accuracy(y, y_hat):
-       return np.sum(y.argmax(axis=1).reshape(-1,1) == y_hat)/len(y)
-
-accuracy(y_train, res)
+accuracy(y_valid, res)
 pd.get_dummies(pd.Series(res.reshape(-1))).sum(axis=0)
